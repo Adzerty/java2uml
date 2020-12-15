4eb56589d4730ed4c5d43be5d9a4cbb8e16615db
Ma2GiK <74398941+Ma2GiK@users.noreply.github.com>
2020-12-15 16:53:00 +0100

    update

---
+++ src/java2uml/IHM/GUI/panelEntite.java
@@ -0,0 +1,63 @@
+package java2uml.IHM.GUI;
+
+import java2uml.metier.*;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class panelEntite extends JPanel {
+	
+	private Entite entite;
+	private PanelAttribut att;
+	private PanelMethode met;
+	public panelEntite(Entite e){
+		
+		this.entite = e;
+		this.att = new PanelAttribut(this.entite.getEnsAttribut());
+		this.met = new PanelMethode(this.entite.getEnsMethode());
+		
+		this.setLayout(new BorderLayout());
+		
+		JPanel pNom    = new JPanel();
+		JPanel pCentre = new JPanel();
+		
+		if(this.entite.getEnsAttribut() == null || this.entite.getEnsMethode() == null)
+		{
+			if(this.entite.getEnsAttribut() == null && this.entite.getEnsMethode() == null)
+			{
+				this.add(pCentre);
+			}
+			
+			else if(this.entite.getEnsAttribut() == null)
+			{
+				pCentre.setLayout(new BorderLayout());
+				pCentre.add(att, "North");
+				pCentre.add(met, "Center");
+			}
+			
+			else if(this.entite.getEnsAttribut() == null)
+			{
+				pCentre.setLayout(new BorderLayout());
+				pCentre.add(att, "Center");
+				pCentre.add(met, "South");
+			}
+		}
+		
+		else
+		{
+			pCentre.setLayout(new GridLayout(2,1));
+			pCentre.add(att);
+			pCentre.add(met);
+		}
+		
+		JLabel nom = new JLabel(this.entite.getNom(), JLabel.CENTER);
+		
+		pNom.add(nom);
+		
+		this.add(pNom, "North");
+		this.add(pCentre, "Center");
+		
+		this.setVisible(true);		
+	}
+}
