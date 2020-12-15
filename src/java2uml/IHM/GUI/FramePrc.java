ea9644f63bbb141cd836bb58089874b2defa7f79
P-Colin <colin.pallier@gmail.com>
2020-12-15 17:38:05 +0100

    Update FramePrc.java

--- src/java2uml/IHM/GUI/FramePrc.java
+++ src/java2uml/IHM/GUI/FramePrc.java
@@ -1,15 +1,27 @@
 package java2uml.IHM.GUI;
 
+import java.util.ArrayList;
+
 import javax.swing.*;
+import java2uml.metier.*;
 
 public class FramePrc extends JFrame {
-
-    public FramePrc() {
+	
+	private PanelPrc panelPrincipal;
+	private ArrayList<Entite> ensEntite = new ArrayList<>();
+	
+    public FramePrc(ConfigReader config) {
+    	
         this.setTitle("Java2UML");
         this.setSize(1200, 600);
         this.setLocation(100, 100);
-
-
+        
+        this.ensEntite = config.getEnsEntite();
+        
+        this.panelPrincipal = new PanelPrc(ensEntite);
+        this.add(this.panelPrincipal);
+        	
+        
         //this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
