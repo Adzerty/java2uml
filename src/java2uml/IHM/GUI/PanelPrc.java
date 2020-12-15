acb3004ae78e3074874ba505b9b30883d8770094
Nathou-byte <63001246+Nathou-byte@users.noreply.github.com>
2020-12-15 17:09:01 +0100

    maj

---
+++ src/java2uml/IHM/GUI/PanelPrc.java
@@ -0,0 +1,13 @@
+package java2uml.IHM.GUI;
+
+import java2uml.metier.*;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class PanelPrc extends JPanel {
+
+	public PanelPrc() {
+		
+	}
+}
