828df29a048ecb96cb4bddc6b2f7fa5c6ec660df
P-Colin <colin.pallier@gmail.com>
2020-12-15 17:38:03 +0100

    Update IHMGUI.java

--- src/java2uml/IHM/GUI/IHMGUI.java
+++ src/java2uml/IHM/GUI/IHMGUI.java
@@ -5,8 +5,11 @@ import java2uml.metier.*;
 public class IHMGUI
 {
 	private ConfigReader conf;
+	private FramePrc framePrincipale;
+
 	public IHMGUI() {
 		this.conf = new ConfigReader("test.config");
+		this.framePrincipale = new FramePrc(this.conf);
 	}
 	
 	public static void main( String[] argv ) {
