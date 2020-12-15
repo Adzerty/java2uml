3d2df377634a417221e342f88f9222cf5e3c7fb4
P-Colin <colin.pallier@gmail.com>
2020-12-15 18:28:29 +0100

    Update PanelEntite.java

--- src/java2uml/IHM/GUI/PanelEntite.java
+++ src/java2uml/IHM/GUI/PanelEntite.java
@@ -2,22 +2,27 @@ package java2uml.IHM.GUI;
 
 import java2uml.metier.*;
 
+
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class PanelEntite extends JPanel {
+public class PanelEntite extends JPanel  {
 	
 	private Entite entite;
 	private PanelAttribut att;
 	private PanelMethode met;
-	public PanelEntite(Entite e){
+	private PanelPrc panelPrincipal;
+	public PanelEntite(Entite e, PanelPrc panelPrincipal){
 		
 		this.entite = e;
+		this.panelPrincipal = panelPrincipal;
 		this.att = new PanelAttribut(this.entite.getEnsAttribut());
 		this.met = new PanelMethode(this.entite.getEnsMethode());
 		
 		this.setLayout(new BorderLayout());
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		
 		JPanel pNom    = new JPanel();
 		JPanel pCentre = new JPanel();
@@ -51,13 +56,17 @@ public class PanelEntite extends JPanel {
 			pCentre.add(met);
 		}
 		
-		JLabel nom = new JLabel(this.entite.getNom(), JLabel.CENTER);
+		JLabel nom = new JLabel(this.entite.getType()+" : "+this.entite.getNom(), JLabel.CENTER);
 		
 		pNom.add(nom);
 		
 		this.add(pNom, "North");
 		this.add(pCentre, "Center");
 		
+		
+		
 		this.setVisible(true);		
 	}
+	
+
 }
