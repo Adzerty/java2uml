b0922f24fbefca8efe3d6d03bd3f37c7747fdfc9
P-Colin <colin.pallier@gmail.com>
2020-12-15 16:35:25 +0100

    Create FramePrc.java

---
+++ src/java2uml/IHM/GUI/FramePrc.java
@@ -0,0 +1,17 @@
+package java2uml.IHM.GUI;
+
+import javax.swing.*;
+
+public class FramePrc extends JFrame {
+
+    public FramePrc() {
+        this.setTitle("Java2UML");
+        this.setSize(1200, 600);
+        this.setLocation(100, 100);
+
+
+        //this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
