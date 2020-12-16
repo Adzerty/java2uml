a1c371968ab44b13041cf18289b768ae1aaf8361
Nathou-byte <63001246+Nathou-byte@users.noreply.github.com>
2020-12-16 11:07:23 +0100

    maj

---
+++ src/java2uml/IHM/GUI/TraitementPosition.java
@@ -0,0 +1,12 @@
+package java2uml.IHM.GUI;
+
+import java.util.ArrayList;
+
+import java2uml.metier.*;
+
+public class TraitementPosition {
+
+	public TraitementPosition( ArrayList<Association> ensAsso, ArrayList<Entite> ensEnite ) {
+		
+	}
+}
