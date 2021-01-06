9063284ee9c32e790209ecc7f33ebd22cdeed83c
Ma2GiK <74398941+Ma2GiK@users.noreply.github.com>
2021-01-06 17:35:06 +0100

    antoine com

--- src/java2uml/IHM/GUI/PanelEntite.java
+++ src/java2uml/IHM/GUI/PanelEntite.java
@@ -11,11 +11,16 @@ import java.util.ArrayList;
 
 public class PanelEntite extends JPanel implements MouseListener  {
 	
+	/*////////////////////////////////////////////////////////////////////////////
+	//                        déclaration des attributs                        //
+	///////////////////////////////////////////////////////////////////////////*/
 	private Entite entite;
 	private PanelAttribut att;
 	private PanelMethode met;
 	private PanelPrc panelPrincipal;
+	
 	private int identifiant;
+	
 	private String nom;
 	private ArrayList<Coord> ensCoord = new ArrayList<>();
 	private String contrainte = "";
@@ -24,6 +29,9 @@ public class PanelEntite extends JPanel implements MouseListener  {
 	
 	public PanelEntite(Entite e, PanelPrc panelPrincipal, int identifiant){
 		
+		/*////////////////////////////////////////////////////////////////////////////
+		//                        attribution des attributs                        //
+		///////////////////////////////////////////////////////////////////////////*/
 		this.entite = e;
 		this.nom = e.getNom();
 		this.panelPrincipal = panelPrincipal;
@@ -31,24 +39,36 @@ public class PanelEntite extends JPanel implements MouseListener  {
 		this.att = new PanelAttribut(this.entite.getEnsAttribut());
 		this.met = new PanelMethode(this.entite.getEnsMethode());
 		
+		
 		this.setLayout(new BorderLayout());
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		
+		
+		// Création fixe si il n'y a pas d'attribut et de méthode
 		JPanel pNom    = new JPanel();
 		JPanel pCentre = new JPanel();
-		if( e.getEnsAttribut().size() == 0 || e.getEnsMethode().size() == 0 ) {
+		if( e.getEnsAttribut().size() == 0 || e.getEnsMethode().size() == 0 ) 
+		{	
 			pCentre.setLayout(new GridLayout(1,1));
 			if( e.getEnsAttribut().size() != 0 )      pCentre.add(att);
-			else if(e.getEnsMethode().size() != 0 ) pCentre.add(met);
+			else if(e.getEnsMethode().size() != 0 )   pCentre.add(met);
 		}
-		else {
+		
+		// Création des panels et de leur taille en fonction des attributs et méthodes
+		else 
+		{
 			pCentre.setLayout(new GridLayout(2,1));
 			pCentre.add(att);
 			pCentre.add(met);
 		}
 		
-		if(this.entite.getContraintes()!=null) for (String cont: this.entite.getContraintes()) contrainte += "{"+cont+"} ";
 		
+		//attribution des contraintes si il y en a
+		if(this.entite.getContraintes()!=null) 
+			for (String cont: this.entite.getContraintes()) contrainte += "{"+cont+"} ";
+		
+		
+		//attribution des types
 		this.entite.setType(this.entite.getType().replaceAll(" ", ""));
 		
 		this.type = this.entite.getType();
@@ -57,9 +77,11 @@ public class PanelEntite extends JPanel implements MouseListener  {
 		if(this.type.equals("Interface")) {this.type = "<<Interface>>";}
 		if(this.type.equals("Classe"))    {this.type = "";}
 		
-		JLabel type = new JLabel(this.type, JLabel.CENTER);
-		JLabel nom = new JLabel(this.entite.getNom()+" "+contrainte, JLabel.CENTER);
+		JLabel type = new JLabel(this.type, JLabel.CENTER); //création du JLabel didié au type
+		JLabel nom = new JLabel(this.entite.getNom()+" "+contrainte, JLabel.CENTER); //création du JLabel didié au nom et contraintes
 
+		
+		
 		pNom.setLayout(new GridLayout(2,1));
 		pNom.add(type);
 		pNom.add(nom);
@@ -72,8 +94,11 @@ public class PanelEntite extends JPanel implements MouseListener  {
 		this.setVisible(true);		
 	}
 	
-	public void setEnsCoord() {
+	public void setEnsCoord() 
+	{
+		//Cette méthode permet de fixer des coordonées sur les classes afin d'y attacher nos liaisons
 		this.ensCoord = new ArrayList<>();
+		
 		this.ensCoord.add(new Coord(this.getX(), this.getY()));
 		this.ensCoord.add(new Coord(this.getX()+(this.getWidth()/2), this.getY()));
 		this.ensCoord.add(new Coord(this.getX()+this.getWidth(), this.getY()));
@@ -98,17 +123,23 @@ public class PanelEntite extends JPanel implements MouseListener  {
 	
 	public Coord[] reflexive()
 	{
+		//Création d'une flèche spéciale pour les classes réfléxives
 		setEnsCoord();
 		Coord[] tab = new Coord[6];
+		
 		tab[0] = new Coord(this.getX(), this.getY()+this.getHeight());
 		tab[1] = new Coord(this.getX(), this.getY()+this.getHeight()+30);
 		tab[2] = new Coord(this.getX()+(this.getWidth()/2), this.getY()+this.getHeight()+30);
 		tab[3] = new Coord(this.getX()+(this.getWidth()/2), this.getY()+this.getHeight());
 		tab[4] = new Coord(tab[0].getX()+5, tab[0].getY()+15);
 		tab[5] = new Coord(tab[3].getX()-30, tab[3].getY()+15);
+		
 		return tab;
 	}
 
+	/*////////////////////////////////////////////////////////////////////////////
+	//Les méthodes qui suivent sont didiées à l'intéraction souris sur le panel//
+	///////////////////////////////////////////////////////////////////////////*/
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
