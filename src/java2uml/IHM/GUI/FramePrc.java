ec837b6d9f41e5cc966edfd4c1c16b25d72ed778
Nathou-byte <63001246+Nathou-byte@users.noreply.github.com>
2021-01-06 17:34:14 +0100

    maj

--- src/java2uml/IHM/GUI/FramePrc.java
+++ src/java2uml/IHM/GUI/FramePrc.java
@@ -1,32 +1,34 @@
+/**
+  Classe : FramePrc
+  @Author : InnovAction
+  @version : 1.0
+  @since : 2021
+*/
 package java2uml.IHM.GUI;
 
 import java.util.ArrayList;
 
 import javax.swing.*;
 
-import java2uml.IHM.GUI.NAVIGATION.MenuBar;
 import java2uml.metier.*;
 
-public class FramePrc extends JFrame {
+public class FramePrc extends JFrame 
+{
 	
 	private PanelPrc panelPrincipal;
 	private ArrayList<Entite> ensEntite = new ArrayList<>();
 	
-	
-    public FramePrc(ConfigReader config, String name) {
-    	
+    public FramePrc(ConfigReader config, String name) 
+    {
         this.setTitle("Java2UML");
         this.setSize(1200, 600);
         this.setLocation(100, 100);
         
         this.ensEntite = config.getEnsEntite();
-       
         
         this.panelPrincipal = new PanelPrc(ensEntite , this, name);
         this.add(this.panelPrincipal);
         	
-        
-        //this.pack();
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setJMenuBar(new MenuBarPrc(this.panelPrincipal));
         this.setVisible(true);
